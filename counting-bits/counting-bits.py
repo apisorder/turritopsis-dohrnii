0958446844426770b70937847602b91b05cd0706
apisorder <apisorder@gmail.com>
2024-05-21 20:47:33 -0700

    counting bits

---
+++ counting-bits/counting-bits.py
@@ -0,0 +1,14 @@
+
+
+# Programmer:     Cheng, Jeff
+# Last Modified:  05-21-2024 08:43PM
+# Problem:        Counting Bits
+
+class Solution:
+    def countBits(self, n):
+        results = [0] * (n+1)
+
+        for i in range(n+1):
+            results[i] = results[i//2] + (i&1)
+
+        return results
