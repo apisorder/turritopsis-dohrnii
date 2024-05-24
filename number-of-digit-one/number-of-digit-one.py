8772e26c642d66ea454a6bd321d59e07e566a9f4
apisorder <apisorder@gmail.com>
2024-05-24 15:25:24 -0700

    number of digit one

--- number-of-digit-one/number-of-digit-one.py
+++ number-of-digit-one/number-of-digit-one.py
@@ -1,5 +1,15 @@
 
 # Programmer:     Cheng, Jeff
-# Last Modified:  05-21-2024 08:43PM
+# Last Modified:  05-24-2024 03:24PM
 # Problem:        Number of Digit Ones
 
+class Solution:
+    def countDigitOne(self, n):
+        multi = 1
+        numOfOnes = 0
+    
+        while multi <= n:
+            numOfOnes += (n//multi + 8)//10 * multi + (n//multi%10==1)*(n%multi+1)
+            multi *= 10
+
+        return numOfOnes
